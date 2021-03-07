620a955738a356cc6db8b17bbf9857544a53e058
Ankit <ankittrajj1996@gmail.com>
2021-03-07 10:38:20 +0530

    update with delete button

--- app.js
+++ app.js
@@ -2,18 +2,26 @@ let submitbtn = document.getElementById('submitbtn');
 let inputtext = document.getElementById('input-text');
 let todo = document.getElementById('todo-container');
 let clearbtn = document.getElementById('clear-btn');
+let delbtn =document.getElementById('deletebtn');
 
 submitbtn.addEventListener('click',()=>{
     let paragraph = document.createElement('p');
     paragraph.innerText=inputtext.value;
     todo.appendChild(paragraph);
     paragraph.classList.add('paragraph')
-    inputtext.value=''
-    // paragraph.style.color='red';
+    inputtext.value='';
+    todo.appendChild(delbtn);
+
+
 
     clearbtn.addEventListener('click',()=>{
         todo.removeChild(paragraph);
-        inputtext.removeChild();
-    })
+        todo.removeChild(delbtn);
+    });
 
-})
+    delbtn.addEventListener('click',()=>{
+        delbtn.removeChild('p');
+    
+    })
+});
+
